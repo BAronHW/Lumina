9f385bef0dc7ac273b39a2340f2bce498ace488b
Aaron <aaronwan808@gmail.com>
2026-06-04 00:20:37 +0100

    working on client repository

--- src/main/scala/com/example/lumina/repository/ClientRepository.scala
+++ src/main/scala/com/example/lumina/repository/ClientRepository.scala
@@ -8,6 +8,14 @@ import java.util.UUID
 class ClientRepository[F[_]] {}
 
 object ClientRepositoryQueries {
-  val insertClient: Command[UUID *: String *: EmptyTuple] =
-    sql"INSERT INTO client (id, name) VALUES ($uuid, $varchar)".command
+  val clientCodec: Codec[Client] = (uuid *: varchar).to[Client]
+
+  val insertClient: Command[Client] =
+    sql"INSERT INTO client (id, name) VALUES $clientCodec".command
+
+  val selectClient: Query[UUID, Client] =
+    sql"SELECT id, name FROM client WHERE id = $uuid".query(clientCodec)
+
+  val updateClient: Command[String *: UUID *: EmptyTuple] =
+    sql"UPDATE client SET name = $varchar WHERE id = $uuid".command
 }
