2cff936295ea54be82b9218e0718a5ddf9842c83
Aaron <aaronwan808@gmail.com>
2026-06-28 02:55:25 +0100

    WIP SpanService.scala

--- src/main/scala/com/example/lumina/services/SpanService.scala
+++ src/main/scala/com/example/lumina/services/SpanService.scala
@@ -1,5 +1,22 @@
 package com.example.lumina.services
 
-trait SpanService {
+import Domain.Span
+import com.example.lumina.repository.SpanRepository
+import skunk.data.Completion
+import java.util.UUID
 
+trait SpanService[F[_]] {
+  def getSpanById(spanId: UUID): F[Option[Span]]
+  def getAllSpans: F[List[Span]]
+  def updateSpan(span: Span): F[Completion]
+  def deleteSpanById(spanId: UUID): F[Completion]
+}
+
+/** This is the service in charge of creating all the spans in this system it should create all spans in buffers
+ *  and should also wait until the buffer reaches a certain size before it creates them in batches by a certain time
+ * */
+object SpanService {
+  def impl[F[_]](ingestBuffer: IngestBuffer[F, Span], spanRepository: SpanRepository[F]): SpanService[F] = new SpanService[F] {
+    
+  }
 }
