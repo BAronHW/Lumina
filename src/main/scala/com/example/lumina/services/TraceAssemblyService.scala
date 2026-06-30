32dc5b00ade864e4c5492660fe7e7a73b2d93e1a
Aaron <aaronwan808@gmail.com>
2026-06-30 21:55:12 +0100

    working on TraceAssemblyService.scala

--- src/main/scala/com/example/lumina/services/TraceAssemblyService.scala
+++ src/main/scala/com/example/lumina/services/TraceAssemblyService.scala
@@ -14,7 +14,7 @@ object TraceAssemblyService {
   def impl[F[_]: Concurrent](
       ingestBuffer: IngestBuffer[F, Span],
       spanService: SpanService[F],
-      @annotation.unused traceService: TraceService[F]
+      traceService: TraceService[F]
   ): TraceAssemblyService[F] = new TraceAssemblyService[F] {
 
     /** This function will take spans contained in the ingest buffer and then generate them using the span service. It
@@ -24,8 +24,9 @@ object TraceAssemblyService {
     override def processSpans(spans: List[Span], chunksToTake: Int): F[Boolean] = {
       for {
         items <- ingestBuffer.tryTakeN(chunksToTake)
-        res <- spanService.createBatchSpan(items)
-      } yield (completionToBool(res)) // WIP
+        containsEndedAt = spanListHasEnd(items)
+
+      } yield () // WIP
     }
 
     /** This function flushes the ingest buffer so that all spans that were in the buffer will now be removed. The
@@ -36,11 +37,25 @@ object TraceAssemblyService {
       ingestBuffer.flushAll
     }
 
+    /** Converts a skunk completion type into boolean by checking if Postgres completion has inserted greater than 0
+      * rows
+      */
     def completionToBool(res: Completion): Boolean = {
       res match {
         case Completion.Insert(n) => n > 0
         case _                    => false
       }
     }
+
+    /** Returns a boolean that represents if the list of span has an element who's endsAt field is defined
+      */
+    def spanListHasEnd(spanList: List[Span]): Boolean = {
+      spanList.exists(span => span.endedAt.isDefined)
+    }
+
+    /** Checks all spans that it comes by if any of them have the endsWithField. If they do we then group all of them
+      * together into a single trace
+      */
+    def createTraceGroup(spanList: List[Span]): F[Completion] = {}
   }
 }
