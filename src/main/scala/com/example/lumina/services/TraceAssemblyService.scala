a91e94a1d6053b9c85a6e2c027b510d31f5bb89f
Aaron <aaronwan808@gmail.com>
2026-06-30 01:19:00 +0100

    adding comments to TraceAssemblyService

--- src/main/scala/com/example/lumina/services/TraceAssemblyService.scala
+++ src/main/scala/com/example/lumina/services/TraceAssemblyService.scala
@@ -9,8 +9,17 @@ trait TraceAssemblyService[F[_]] {
 
 object TraceAssemblyService {
   def impl[F[_]](ingestBuffer: IngestBuffer[F, Span]): TraceAssemblyService[F] = new TraceAssemblyService[F] {
+
+    /** This function will take spans contained in the ingest buffer and then generate them using the span service. It
+      * will also use the trace service to group spans together when the final span arrives which is denoted if a span
+      * has an ended at field that is not null.
+      */
     override def processSpans(spans: List[Span]): F[Boolean] = ???
 
+    /** This function flushes the ingest buffer so that all spans that were in the buffer will now be removed. The
+      * function will return true if the buffer is flushed and will return false if it cannot be flushed or if the
+      * buffer originally had nothing to flush.
+      */
     override def flush: F[Boolean] = {
       ingestBuffer.flushAll
     }
