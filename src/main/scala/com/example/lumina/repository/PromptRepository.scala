5c9b23bd4efcc4166562c94b3bda124b192c4115
Aaron <aaronwan808@gmail.com>
2026-07-04 22:55:58 +0100

    Working on Prompts

--- src/main/scala/com/example/lumina/repository/PromptRepository.scala
+++ src/main/scala/com/example/lumina/repository/PromptRepository.scala
@@ -6,7 +6,7 @@ import cats.effect.kernel.Resource
 import skunk.codec.all.{uuid, varchar}
 import skunk.data.Completion
 import skunk.implicits.sql
-import skunk.{Command, Session}
+import skunk.{Codec, Command, Query, Session}
 import cats.syntax.all.*
 
 import java.util.UUID
@@ -19,9 +19,18 @@ class PromptRepository[F[_]: Concurrent](session: Resource[F, Session[F]]) {
     }
   }
 
+  def deletePrompt(id: UUID): F[Completion] = {
+    session.use { s =>
+      s.prepare(PromptRepositoryQueries.deletePrompt).flatMap(ps => ps.execute(id))
+    }
+  }
+
   private object PromptRepositoryQueries {
     val promptEnc = (uuid *: varchar *: varchar).values.to[Prompt]
+    val promptDec: Codec[Prompt] = (uuid *: varchar *: varchar).to[Prompt]
     val createPrompt: Command[Prompt] = sql"INSERT INTO prompt (id, name, prompt) VALUES $promptEnc".command
     val deletePrompt: Command[UUID] = sql"DELETE FROM prompt where id = $uuid".command
+    val selectPromptWithId: Query[UUID, Prompt] = sql"SELECT * FROM trace WHERE id = $uuid".query(promptDec)
+
   }
 }
