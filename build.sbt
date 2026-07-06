1d92df798a0c9af3612db4c8391a860a1741d0af
Aaron <aaronwan808@gmail.com>
2026-07-06 19:26:17 +0100

    fix

--- build.sbt
+++ build.sbt
@@ -22,6 +22,7 @@ libraryDependencies ++= Seq(
   "io.circe" %% "circe-core" % circeVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "org.typelevel" %% "log4cats-slf4j" % "2.8.0",
+  "org.typelevel" %% "log4cats-core" % "2.8.0",
   "io.circe" %% "circe-parser" % circeVersion,
   "org.tpolecat" %% "skunk-core" % skunkVersion,
   "org.tpolecat" %% "skunk-circe" % skunkVersion,
--- src/main/scala/com/example/lumina/services/SpanQueueWorker.scala
+++ src/main/scala/com/example/lumina/services/SpanQueueWorker.scala
@@ -1,7 +1,6 @@
 package com.example.lumina.services
 
 import cats.effect.Temporal
-import cats.effect.std.Console
 import cats.syntax.all.*
 import com.example.lumina.types.WorkerConfig
 import fs2.Stream
@@ -15,14 +14,13 @@ trait SpanQueueWorker[F[_]] {
 /** This span queue worker creates a stream that periodically polls the ingest buffer and creates spans
   */
 object SpanQueueWorker {
-  def impl[F[_]: Temporal: Console](
+  def impl[F[_]: Temporal](
       traceAssemblyService: TraceAssemblyService[F],
       workerConfig: WorkerConfig
   ): SpanQueueWorker[F] = new SpanQueueWorker[F] {
     override def stream: Stream[F, Unit] =
       Stream
         .awakeEvery[F](workerConfig.pollInterval.millis)
-        .evalTap(_ => Console[F].println("POLLING HERE"))
         .evalMap(_ => traceAssemblyService.processSpans(workerConfig.chunkSize))
         .void
   }
