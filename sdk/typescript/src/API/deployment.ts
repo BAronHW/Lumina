1e24a1835707c34ed922d8448cb21b2e8d1b2559
Aaron <aaronwan808@gmail.com>
2026-07-15 02:09:56 +0100

    WIP

--- sdk/typescript/src/API/deployment.ts
+++
@@ -1,8 +0,0 @@
-import { UUID } from "node:crypto"
-
-export interface Deployment {
-    id: UUID
-    clientId: UUID
-    version: string
-    deployedAt: Date
-}
--- sdk/typescript/src/API/deployment/deployment.ts
+++ sdk/typescript/src/API/deployment/deployment.ts
@@ -2,5 +2,7 @@ import { UUID } from "node:crypto"
 
 export interface Deployment {
     id: UUID
-    name: string
+    clientId: UUID
+    version: string
+    deployedAt: Date
 }
