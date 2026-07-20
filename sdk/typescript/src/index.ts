536f6e7aa32021dd89042577ad827e3dcb456b51
Aaron <aaronwan808@gmail.com>
2026-07-21 00:56:04 +0100

    exporting things available to user at index.ts

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -1,5 +1,8 @@
-const test = () => {
-    console.log('hello');
-}
+export { LuminaSDKImpl } from './sdk/lumina-sdk/lumina-sdk'
 
-test();
+export type { LuminaSDK } from './api/tracer-sdk'
+export type { StartTraceBody } from './api/trace/trace'
+export type { StartSpanBody } from './api/span/span'
+export type { SpanKind, SpanStatus } from './api/types'
+export type { Trace } from './api/trace/trace'
+export type { Span } from './api/span/span'
