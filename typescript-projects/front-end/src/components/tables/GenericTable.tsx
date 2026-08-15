6c82030998f7c28c98a1c8f2977ea6852d5a44e1
Aaron <aaronwan808@gmail.com>
2026-08-15 20:48:44 +0100

    generic table component

--- typescript-projects/front-end/src/components/tables/GenericTable.tsx
+++ typescript-projects/front-end/src/components/tables/GenericTable.tsx
@@ -1,20 +1,13 @@
 import { Table } from '@mantine/core';
 
-interface TableHeader {
-  tableColumn: string;
+interface ColumnDef<T> {
+  header: string;
+  accessor: (row: T) => React.ReactNode;
 }
 
-interface TableRow<RowType> {
-  tableRow: RowType
-}
-
-interface Row<> {
-  
-}
-
-interface TableProps {
-  header: TableHeader[];
-  rows: TableRow<string>[];
+interface TableProps<T> {
+  columns: ColumnDef<T>[];
+  rows: T[];
 }
 
 /**
@@ -24,34 +17,26 @@ interface TableProps {
  * which represents 
  * @returns 
  */
-export function GenericTable() {
-    const elements = [
-        { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
-        { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-        { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-        { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-        { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
-    ];
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
-      <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
-    </Table.Tr>
-  ));
+export function GenericTable<T>({ columns, rows }: TableProps<T>) {
 
   return (
     <Table>
       <Table.Thead>
         <Table.Tr>
-          <Table.Th>Element position</Table.Th>
-          <Table.Th>Element name</Table.Th>
-          <Table.Th>Symbol</Table.Th>
-          <Table.Th>Atomic mass</Table.Th>
+          {columns.map((col) => (
+            <Table.Th key={col.header}>{ col.header }</Table.Th>
+            ))}
         </Table.Tr>
       </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
+      <Table.Tbody>
+        {rows.map((row, index) => (
+          <Table.Tr key={index}>
+            {columns.map((col) => (
+              <Table.Td key={col.header}>{col.accessor(row)}</Table.Td>
+              ))}
+          </Table.Tr>
+          ))}
+      </Table.Tbody>
     </Table>
   );
 }
