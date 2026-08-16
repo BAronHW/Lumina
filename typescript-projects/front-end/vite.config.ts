2e1e3ed589dcb4376df8716748b05b01e761f3cd
Aaron <aaronwan808@gmail.com>
2026-08-16 23:34:06 +0100

    adding proxy to api

--- typescript-projects/front-end/vite.config.ts
+++ typescript-projects/front-end/vite.config.ts
@@ -4,6 +4,11 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    }
+  },
   plugins: [
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
