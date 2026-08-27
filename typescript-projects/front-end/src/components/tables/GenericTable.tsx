1a458c76b360633705bde3f06803c3e0588d32f2
Aaron <aaronwan808@gmail.com>
2026-08-27 23:41:50 +0100

    generic table

--- typescript-projects/front-end/src/components/tables/GenericTable.tsx
+++ typescript-projects/front-end/src/components/tables/GenericTable.tsx
@@ -8,16 +8,10 @@ interface ColumnDef<T> {
 interface TableProps<T> {
   columns: ColumnDef<T>[];
   rows: T[];
+  getRowKey: (row: T) => string;
 }
 
-/**
- * This component is a generic table that can be used for all purposes of this application
- * The component accepts props of an array of TableRow which is represents the headers
- * of the table. On top of this there is the TableRow prop which is an array of objects
- * which represents 
- * @returns 
- */
-export function GenericTable<T>({ columns, rows }: TableProps<T>) {
+export function GenericTable<T>({ columns, rows, getRowKey }: TableProps<T>) {
 
   return (
     <Table>
@@ -29,8 +23,8 @@ export function GenericTable<T>({ columns, rows }: TableProps<T>) {
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
-        {rows.map((row, index) => (
-          <Table.Tr key={index}>
+        {rows.map((row) => (
+          <Table.Tr key={getRowKey(row)}>
             {columns.map((col) => (
               <Table.Td key={col.header}>{col.accessor(row)}</Table.Td>
               ))}
