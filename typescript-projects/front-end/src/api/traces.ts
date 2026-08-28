afb1ca255313077cdc7d0aa6759e9eef2e65be6e
Aaron <aaronwan808@gmail.com>
2026-08-29 00:01:03 +0100

    adding search parameters to traces api call

--- typescript-projects/front-end/src/api/traces.ts
+++ typescript-projects/front-end/src/api/traces.ts
@@ -1,8 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
-export function useTraces() {
+export function useTraces(page?: number, pageSize?: number) {
+    const params = new URLSearchParams();
+    if (page !== undefined) params.set('page', page.toString());
+    if (pageSize !== undefined) params.set('pageSize', pageSize.toString());
+    const query = params.toString();
+
     return useQuery({
-        queryKey: ['traces'],
-        queryFn: () => fetch('/api/traces').then((res) => res.json())
+        queryKey: ['traces', { page, pageSize }],
+        queryFn: () => fetch(`/api/traces${query ? `?${query}` : ''}`).then((res) => res.json())
     })
 }
